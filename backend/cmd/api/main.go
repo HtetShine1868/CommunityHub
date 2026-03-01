0873b938e2a31526951f2646addb58e8ffa29cbe
HtetShine1868 <hetaungshine25@gmail.com>
2026-03-02 01:09:40 +0630

    remove unused

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,7 +3,6 @@ package main
 import (
     "communityHub/internal/handlers"
     "communityHub/internal/middleware"
-    "communityHub/internal/models"
     "communityHub/internal/repository"
     "fmt"
     "log"
